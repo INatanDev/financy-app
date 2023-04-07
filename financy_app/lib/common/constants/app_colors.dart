1bd44a9621ad6ad3ec3980d27999e3e6c2abd3a1
INatanDev <natan.ferreira21@hotmail.com>
2023-04-07 17:41:13 -0300

    feito classes de cores e estilho para page splash

---
+++ financy_app/lib/common/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color greeLightOne = Color(0xFF63B5AF);
+  static const Color greeLighttwo = Color(0xFF438883);
+  static const List<Color> greenGradient = [
+    Color(0xFF63B5AF),
+    Color(0xFF438883),
+  ];
+  static const Color white = Color(0xFFFFFFFF);
+}
